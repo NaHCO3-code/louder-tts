a4aba2a34ed1d380a5facf7eacbfff236518efd8
surfish <shrshr2@163.com>
2025-03-23 17:00:26 +0800

    临时上线

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   base: '/continuation-writing-helper/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [
     tailwindcss(),
   ],
